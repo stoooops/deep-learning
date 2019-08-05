4640276551dae0c88920f3a1712f1bbe7b18faa9
Cory Gabrielsen <corygabrielsen@gmail.com>
2019-08-05 09:59:57 -0700

    Add resnet50 notebook

---
+++ utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+def print_list(l):
+    print('\n'.join([str(item) for item in l]))
+
+def properties_names(obj):
+    return sorted([k for k, v in obj.__dict__.items()])
+
