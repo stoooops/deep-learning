06c0c4af348dfcafe45d81d486697af5782b426c
Cory Gabrielsen <coryg@maka-ars.com>
2019-08-07 15:44:56 -0700

    Fix SRC_DIR definition, add TMP_DIR

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -3,9 +3,13 @@
 import os
 
 
-SRC_DIR = os.path.dirname(os.path.realpath(__file__))
-_EXPECTED_END_SRC_DIR = 'src/utils'
-assert SRC_DIR[-len(_EXPECTED_END_SRC_DIR):] == _EXPECTED_END_SRC_DIR
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+_EXPECTED_END_FILE_DIR = 'src/utils'
+assert FILE_DIR[-len(_EXPECTED_END_FILE_DIR):] == _EXPECTED_END_FILE_DIR
 
-MODELS_DIR = os.path.normpath(os.path.join(SRC_DIR, '../../models'))
+SRC_DIR = os.path.normpath(os.path.join(FILE_DIR, '..'))
+
+MODELS_DIR = os.path.normpath(os.path.join(SRC_DIR, '../models'))
+
+TMP_DIR = os.path.normpath(os.path.join(SRC_DIR, '../tmp'))
 
