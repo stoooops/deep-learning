f220bdd40fc5c2fc6b166b7fdaf1c553b365015d
Cory Gabrielsen <coryg@maka-ars.com>
2019-08-29 10:20:08 -0700

    Allow overriding interval in bin/gpu.sh

--- bin/gpu.sh
+++ bin/gpu.sh
@@ -5,7 +5,7 @@ NVIDIA_HEADER="timestamp,name,pci.bus_id,driver_version,pstate,pcie.link.gen.max
 
 nvidia-smi --query-gpu=$NVIDIA_HEADER --format=csv | column -s , -t
 while true; do
-    sleep 5
+    sleep ${1:-5}
     nvidia-smi --query-gpu=$NVIDIA_HEADER --format=csv | column -s , -t | tail -1
 done
 
